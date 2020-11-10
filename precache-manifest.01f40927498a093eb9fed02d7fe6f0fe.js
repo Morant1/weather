079e084b23c4d3d83b5d56fcfcd9a704d0cc376b
Morant1 <moran8865@walla.com>
2020-11-10 15:04:03 +0200

    Updates

---
+++ precache-manifest.01f40927498a093eb9fed02d7fe6f0fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950f12c2fae31893de81586eca7dd78b",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "f9300c4262580a13fcd9",
+    "url": "/weather/static/css/main.943f9609.chunk.css"
+  },
+  {
+    "revision": "df994c02a952160be27e",
+    "url": "/weather/static/js/2.f7a0d1ee.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/weather/static/js/2.f7a0d1ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9300c4262580a13fcd9",
+    "url": "/weather/static/js/main.05f82e27.chunk.js"
+  },
+  {
+    "revision": "1557ce1959b3418744e2",
+    "url": "/weather/static/js/runtime-main.80a89714.js"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/weather/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/weather/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "528f75c9641b1a73c4881a1e28dcdaec",
+    "url": "/weather/static/media/icon.528f75c9.png"
+  }
+]);
